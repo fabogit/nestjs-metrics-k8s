4f5a287884b8472e1f1ef25ccb2d8bf15c054e77
fabogit <fabo.dev.email@gmail.com>
2025-02-19 11:38:17 +0100

    #2
    added JSDoc

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,34 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './logging.interceptor';
 
 @Module({
+  /**
+   * Here, we are importing `PrometheusModule.register()` to enable Prometheus metrics
+   * in this application module. `PrometheusModule.register()` configures
+   * and sets up the Prometheus module to expose http://localhost:3000/metrics.
+   *
+   * By importing `PrometheusModule`, components within AppModule can utilize
+   * services and functionalities provided by the Prometheus module for metrics exposition.
+   */
   imports: [PrometheusModule.register()],
   controllers: [AppController],
+  /**
+   * providers: Array of providers that are instantiated by the NestJS injector
+   * and are available for injection within this module. Providers can be services,
+   * interceptors, guards, etc.
+   *
+   *
+   * - `{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }`: This is a custom
+   *   provider configuration.
+   *     - `provide: APP_INTERCEPTOR`:  Specifies the token (key) for this provider.
+   *       `APP_INTERCEPTOR` is a special token from `@nestjs/core` that tells NestJS
+   *       to use this provider as a global interceptor for the application.
+   *     - `useClass: LoggingInterceptor`:  Instructs NestJS to instantiate and use
+   *       the `LoggingInterceptor` class as the provider for `APP_INTERCEPTOR`.
+   *
+   *   This configuration globally registers `LoggingInterceptor` as an
+   *   application-wide interceptor, meaning it will be executed for every route handler
+   *   in the application.
+   */
   providers: [
     AppService,
     { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -10,12 +10,41 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { v4 as uuidv4 } from 'uuid';
 
+/**
+ * @Injectable decorator marks this class as a provider that can be injected
+ * into other classes or modules within the NestJS application.
+ *
+ * The `LoggingInterceptor` class implements the `NestInterceptor` interface,
+ * allowing it to intercept and modify incoming and outgoing `Requests` & `Responses`
+ * in the application's request pipeline.
+ */
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  /**
+   * @private
+   * @readonly
+   * logger: Logger instance for logging messages within this interceptor.
+   * It is initialized with the name of the `LoggingInterceptor` class and configured
+   * to include timestamps in the log messages.
+   */
   private readonly logger = new Logger(LoggingInterceptor.name, {
     timestamp: true,
   });
 
+  /**
+   * Intercepts incoming `Requests` and outgoing `Responses`.
+   * This method is the core of the interceptor and is automatically called by NestJS
+   * for each request that goes through the application's request pipeline and is
+   * associated with this interceptor.
+   *
+   * @param {ExecutionContext} context - Provides access to the execution context of the request,
+   *                                     including details about the controller, handler, and arguments.
+   * @param {CallHandler} next -  Represents the next interceptor or the route handler itself in the chain.
+   *                              Calling `next.handle()` proceeds with the request handling pipeline.
+   * @returns {Observable<any>} Returns an Observable that represents the flow of the request/response
+   *                             handling. The interceptor can modify this observable to tap into or
+   *                             transform the data stream.
+   */
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     if (context.getType() === 'http') {
       return this.logHttpCall(context, next);
@@ -23,9 +52,25 @@ export class LoggingInterceptor implements NestInterceptor {
     return next.handle();
   }
 
-  private logHttpCall(context: ExecutionContext, next: CallHandler) {
+  /**
+   * @private
+   * Logs HTTP `Request` and `Response` details.
+   * This method is called by the `intercept` method when the context type is 'http'.
+   * It extracts relevant information from the `request` and `response` objects to log
+   * details about the HTTP call, including request method, URL, user agent, IP address,
+   * controller and handler names, correlation key, response status code, and content length.
+   *
+   * @param {ExecutionContext} context - The execution context providing access to HTTP request/response.
+   * @param {CallHandler} next - The call handler to proceed with the request processing.
+   * @returns {Observable<any>} Returns an Observable that represents the flow of the HTTP request/response
+   *                             handling, allowing the interceptor to tap into the data stream for logging.
+   */
+  private logHttpCall(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> {
     const request: Request = context.switchToHttp().getRequest();
-    const userAgent = request.get('user-agent') || '';
+    const userAgent = request.get('user-agent');
     const { ip, method, path: url } = request;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     const correlationKey = uuidv4() as string;
